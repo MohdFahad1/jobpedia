fb8a1bf9f9a2ae6f240f904e9ffe64f43b74649f
MohdFahad1 <98938137+MohdFahad1@users.noreply.github.com>
2023-08-17 22:47:38 +0530

    minor changes in category section

--- components/Home/Categories/Category.js
+++ components/Home/Categories/Category.js
@@ -8,56 +8,56 @@ import {PiGraduationCap, PiPenNibBold} from 'react-icons/pi'
 const Category = () => {
     return (
         <section className="bg-[#E0EEF3] px-2 py-10 md:px-20 lg:px-20">
-            <h1 className="text-center text-3xl font-semibold">Popular Job Categories</h1>
+            <h1 className="text-center text-3xl font-semibold lg:text-4xl">Popular Job Categories</h1>
             <div className="flex justify-center items-center">
-                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-10 mt-7">
+                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-10 mt-10">
 
-                <div className="p-4 bg-white w-[300px] h-[300px] rounded-md shadow-[0_3px_10px_rgb(0,0,0,0.2)] cursor-pointer hover:bg-[#20B8DE] hover:text-white hover:content duration-300 text-center flex flex-col justify-center items-center">
-                <span className="text-6xl   "><FaFigma /></span>
+                <div className="p-4 bg-white w-[270px] h-[250px] rounded-md shadow-[0_3px_10px_rgb(0,0,0,0.2)] cursor-pointer hover:bg-[#20B8DE] hover:text-white hover:content duration-300 flex flex-col justify-center">
+                <span className="text-5xl   bg-[#81ddf5] rounded-full p-2 flex items-center justify-center w-[100px] h-[100px]"><FaFigma /></span>
                         <h1 className="text-2xl font-semibold my-4">Design & Art</h1>
-                        <p>100+ jobs vacancy in a day</p>
+                        <p>100+ jobs vacancy</p>
                     </div>
 
-                        <div className="p-4 bg-white w-[300px] h-[300px] rounded-md shadow-[0_3px_10px_rgb(0,0,0,0.2)] cursor-pointer hover:bg-[#20B8DE] hover:text-white duration-300 text-center flex flex-col justify-center items-center">
-                        <span className="text-6xl   "><FaCode /></span>
+                        <div className="p-4 bg-white w-[270px]   h-[250px] rounded-md shadow-[0_3px_10px_rgb(0,0,0,0.2)] cursor-pointer hover:bg-[#20B8DE] hover:text-white duration-300 flex flex-col justify-center">
+                        <span className="text-5xl   bg-[#81ddf5] rounded-full p-2 flex items-center justify-center w-[100px] h-[100px]"><FaCode /></span>
                             <h1 className="text-2xl font-semibold my-4">Development</h1>
-                            <p>100+ jobs vacancy in a day</p>
+                            <p>100+ jobs vacancy</p>
                         </div>
 
-                        <div className="p-4 bg-white w-[300px] h-[300px] rounded-md shadow-[0_3px_10px_rgb(0,0,0,0.2)] cursor-pointer hover:bg-[#20B8DE] hover:text-white duration-300 text-center flex flex-col justify-center items-center">
-                            <span className="text-6xl   "><GoGoal /></span>
+                        <div className="p-4 bg-white w-[270px]   h-[250px] rounded-md shadow-[0_3px_10px_rgb(0,0,0,0.2)] cursor-pointer hover:bg-[#20B8DE] hover:text-white duration-300 flex flex-col justify-center">
+                            <span className="text-5xl   bg-[#81ddf5] rounded-full p-2 flex items-center justify-center w-[100px] h-[100px]"><GoGoal /></span>
                             <h1 className="text-2xl font-semibold my-4">Human Resource</h1>
-                            <p>100+ jobs vacancy in a day</p>
+                            <p>100+ jobs vacancy</p>
                         </div>
 
-                        <div className="p-4 bg-white w-[300px] h-[300px] rounded-md shadow-[0_3px_10px_rgb(0,0,0,0.2)] cursor-pointer hover:bg-[#20B8DE] hover:text-white duration-300 text-center flex flex-col justify-center items-center">
-                        <span className="text-6xl   "><ImStatsBars /></span>
+                        <div className="p-4 bg-white w-[270px]   h-[250px] rounded-md shadow-[0_3px_10px_rgb(0,0,0,0.2)] cursor-pointer hover:bg-[#20B8DE] hover:text-white duration-300 flex flex-col justify-center">
+                        <span className="text-5xl bg-[#81ddf5] rounded-full p-2 flex items-center justify-center w-[100px] h-[100px]"><ImStatsBars /></span>
                             <h1 className="text-2xl font-semibold my-4">Sales & Marketing</h1>
-                            <p>100+ jobs vacancy in a day</p>
+                            <p>100+ jobs vacancy</p>
                         </div>
 
-                        <div className="p-4 bg-white w-[300px] h-[300px] rounded-md shadow-[0_3px_10px_rgb(0,0,0,0.2)] cursor-pointer hover:bg-[#20B8DE] hover:text-white duration-300 text-center flex flex-col justify-center items-center">
-                        <span className="text-6xl   "><GoDeviceCameraVideo /></span>
+                        <div className="p-4 bg-white w-[270px]   h-[250px] rounded-md shadow-[0_3px_10px_rgb(0,0,0,0.2)] cursor-pointer hover:bg-[#20B8DE] hover:text-white duration-300 flex flex-col justify-center">
+                        <span className="text-5xl bg-[#81ddf5] rounded-full p-2 flex items-center justify-center w-[100px] h-[100px] "><GoDeviceCameraVideo /></span>
                             <h1 className="text-2xl font-semibold my-4">Video Editting</h1>
-                            <p>100+ jobs vacancy in a day</p>
+                            <p>100+ jobs vacancy</p>
                         </div>
 
-                        <div className="p-4 bg-white w-[300px] h-[300px] rounded-md shadow-[0_3px_10px_rgb(0,0,0,0.2)] cursor-pointer hover:bg-[#20B8DE] hover:text-white duration-300 text-center flex flex-col justify-center items-center">
-                        <span className="text-6xl   "><HiOutlineSpeakerphone /></span>
+                        <div className="p-4 bg-white w-[270px]   h-[250px] rounded-md shadow-[0_3px_10px_rgb(0,0,0,0.2)] cursor-pointer hover:bg-[#20B8DE] hover:text-white duration-300 flex flex-col justify-center">
+                        <span className="text-5xl bg-[#81ddf5] rounded-full p-2 flex items-center justify-center w-[100px] h-[100px]"><HiOutlineSpeakerphone /></span>
                             <h1 className="text-2xl font-semibold my-4">Marketing</h1>
-                            <p>100+ jobs vacancy in a day</p>
+                            <p>100+ jobs vacancy</p>
                         </div>
 
-                        <div className="p-4 bg-white w-[300px] h-[300px] rounded-md shadow-[0_3px_10px_rgb(0,0,0,0.2)] cursor-pointer hover:bg-[#20B8DE] hover:text-white duration-300 text-center flex flex-col justify-center items-center">
-                        <span className="text-6xl   "><PiGraduationCap /></span>
+                        <div className="p-4 bg-white w-[270px]   h-[250px] rounded-md shadow-[0_3px_10px_rgb(0,0,0,0.2)] cursor-pointer hover:bg-[#20B8DE] hover:text-white duration-300 flex flex-col justify-center">
+                        <span className="text-5xl bg-[#81ddf5] rounded-full p-2 flex items-center justify-center w-[100px] h-[100px]"><PiGraduationCap /></span>
                             <h1 className="text-2xl font-semibold my-4">Education</h1>
-                            <p>100+ jobs vacancy in a day</p>
+                            <p>100+ jobs vacancy</p>
                         </div>
 
-                        <div className="p-4 bg-white w-[300px] h-[300px] rounded-md shadow-[0_3px_10px_rgb(0,0,0,0.2)] cursor-pointer hover:bg-[#20B8DE] hover:text-white duration-300 text-center flex flex-col justify-center items-center">
-                        <span className="text-6xl   "><PiPenNibBold /></span>
+                        <div className="p-4 bg-white w-[270px]   h-[250px] rounded-md shadow-[0_3px_10px_rgb(0,0,0,0.2)] cursor-pointer hover:bg-[#20B8DE] hover:text-white duration-300 flex flex-col justify-center">
+                        <span className="text-5xl bg-[#81ddf5] rounded-full p-2 flex items-center justify-center w-[100px] h-[100px]"><PiPenNibBold /></span>
                             <h1 className="text-2xl font-semibold my-4">Digital & Creative</h1>
-                            <p>100+ jobs vacancy in a day</p>
+                            <p>100+ jobs vacancy</p>
                         </div>
                 </div>
             </div>
