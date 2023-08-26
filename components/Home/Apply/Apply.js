a7e7c54821ffb68d1d0f91a10a9afd5fd097bbb3
MohdFahad1 <98938137+MohdFahad1@users.noreply.github.com>
2023-08-26 14:57:57 +0530

    Changed the UI of home page second comp.

--- components/Home/Apply/Apply.js
+++ components/Home/Apply/Apply.js
@@ -1,35 +1,50 @@
 import React from 'react';
+import Image from 'next/image';
 import { RiAccountCircleLine } from 'react-icons/ri';
 import { TiTick } from 'react-icons/ti';
 import { IoNewspaperOutline } from 'react-icons/io5';
+import apply from '../../../assets/applyLady.png';
 
 const Apply = () => {
   return (
-    <section className="px-5 my-10 md:my-14 lg:px-20 ">
-      <div>
-        <h1 className="text-3xl font-bold text-center mb-7 md:text-4xl lg:text-5xl">Just 3 Easy Steps to New Capabilities</h1>
+    <section className="flex md:flex-col lg:flex-row lg:justify-around justify-center items-center px-5 my-5 md:my-14 lg:px-0">
+      <div className="bg-[#20b8DE] lg:flex justify-end rounded-tl-[248px] rounded-tr-[201px] rounded-bl-[150px] rounded-b-[110px]  h-[450px] w-[450px] hidden">
+        <Image src={apply} alt="human" className="hidden md:block lg:block -scale-x-100  -mr-[100px] h-[450px] w-[600px]" />
       </div>
-      <div className="flex flex-col gap-10 justify-between items-center md:flex-row lg:flex-row lg:gap-0">
-        <div className="flex flex-col gap-2 justify-center items-center text-center p-3 w-[250px] h-[250px] shadow-[0px_2px_43px_3px_#00000024] rounded-md">
-            <div  className="text-5xl bg-[#20B8DE] text-white rounded-full h-[80px] w-[60px] flex justify-center items-center">
-                <RiAccountCircleLine />
+
+      <div className='w-auto lg:w-[500px]'>
+        <h1 className="text-2xl font-bold text-center md:text-4xl lg:text-4xl lg:text-left mb-3 md:mb-5 lg:mb-10">Just 3 Easy Steps to New Capabilities</h1>
+
+        <div className="flex flex-col gap-5">
+          <div className="flex gap-5 justify-start items-center p-3  shadow-[0_3px_10px_rgb(0,0,0,0.2)] rounded-md">
+            <div className="text-5xl bg-[#20B8DE] text-white p-2 flex justify-center items-center rounded-full">
+              <RiAccountCircleLine />
             </div>
-            <h1 className="text-xl font-semibold">Account</h1>
-            <p className="text-gray-500">Join us today and step into a brighter professional future.</p>
-        </div>
-        <div className="flex flex-col gap-2 justify-center items-center text-center p-3 w-[250px] h-[250px] shadow-[0px_2px_43px_3px_#00000024] rounded-md">
-            <div  className="text-5xl bg-[#20B8DE] text-white rounded-full h-[80px] w-[60px] flex justify-center items-center">
-                <IoNewspaperOutline />
+            <div>
+              <h1 className="text-xl font-semibold mb-2">Create Account</h1>
+              <p className="text-gray-500">Join us today and step into a brighter professional future.</p>
             </div>
-            <h1 className="text-xl font-semibold">Quick Jobs</h1>
-            <p className="text-gray-500">Find tailored job opportunities that fit your goals.</p>
-        </div>
-        <div className="flex flex-col gap-2 justify-center items-center text-center p-3 w-[250px] h-[250px] shadow-[0px_2px_43px_3px_#00000024] rounded-md">
-            <div  className="text-5xl bg-[#20B8DE] text-white rounded-full h-[80px] w-[60px] flex justify-center items-center">
-                <TiTick />
+          </div>
+
+          <div className="flex gap-5 justify-start items-center p-3  shadow-[0_3px_10px_rgb(0,0,0,0.2)] rounded-md">
+            <div className="text-5xl bg-[#20B8DE] text-white p-2 flex justify-center items-center rounded-full">
+              <IoNewspaperOutline />
             </div>
-            <h1 className="text-xl font-semibold">Apply Them</h1>
-            <p className="text-gray-500">Unleash your career potential with our job searching site.</p>
+            <div>
+              <h1 className="text-xl font-semibold mb-2">Quick Jobs</h1>
+              <p className="text-gray-500">Find tailored job opportunities that fit your goals.</p>
+            </div>
+          </div>
+
+          <div className="flex gap-5 justify-start items-center p-3  shadow-[0_3px_10px_rgb(0,0,0,0.2)] rounded-md">
+            <div className="text-5xl bg-[#20B8DE] text-white p-2  flex justify-center items-center rounded-full">
+              <TiTick />
+            </div>
+            <div>
+              <h1 className="text-xl font-semibold mb-2">Apply Them</h1>
+              <p className="text-gray-500">Unleash your career potential with our job searching site.</p>
+            </div>
+          </div>
         </div>
       </div>
     </section>
