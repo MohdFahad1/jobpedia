37d29c43c324fef3042ba10beb6645b8250ad4ce
MohdFahad1 <98938137+MohdFahad1@users.noreply.github.com>
2023-08-27 23:20:56 +0530

    Added Pagination & Validations in jobs page

--- components/Navbar/Navbar.js
+++ components/Navbar/Navbar.js
@@ -8,11 +8,11 @@ const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <nav className="flex lg:justify-around lg:px-[0] items-center text-lg font-semibold justify-between px-5 bg-transparent h-[80px] shadow-sm">
+    <nav className="flex lg:justify-around lg:px-[0] items-center text-lg font-semibold justify-between px-5 bg-transparent h-[80px] shadow-sm z-10">
       <Link href="/">
         <Image src={logo} alt="logo" className=" bg-transparent h-[60px] w-[70px] mt-2" />
       </Link>
-      <ul className={`lg:static lg:flex lg:flex-row lg:gap-0 lg:justify-around lg:w-[600px] ${isOpen ? "block" : "hidden"} w-auto top-20 right-4 p-3 flex flex-col gap-6 absolute bg-white lg:shadow-none shadow-[0px_4px_16px_rgba(17,17,26,0.1),_0px_8px_24px_rgba(17,17,26,0.1),_0px_16px_56px_rgba(17,17,26,0.1)]`}>
+      <ul className={`lg:static lg:flex lg:flex-row lg:gap-0 lg:justify-around lg:w-[600px] ${isOpen ? "block" : "hidden"} w-auto top-20 right-4 p-3 flex flex-col gap-6 absolute bg-white lg:shadow-none shadow-[0px_4px_16px_rgba(17,17,26,0.1),_0px_8px_24px_rgba(17,17,26,0.1),_0px_16px_56px_rgba(17,17,26,0.1)] z-20`}>
         <Link href="/" onClick={()=>setIsOpen(false)}><li className="hover:text-[#4374C2] duration-300">Home</li></Link>
         <Link href="/jobs" onClick={()=>setIsOpen(false)}><li className="hover:text-[#4374C2] duration-300">Find Jobs</li></Link>
         <Link href="/about" onClick={()=>setIsOpen(false)}><li className="hover:text-[#4374C2] duration-300">About Us</li></Link>
